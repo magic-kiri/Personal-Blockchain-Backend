499cc109a7d35fe326a51dfc7be01d34c1c19b52
magickiri <kimuapon@gmail.com>
2021-04-02 13:23:47 +0600

    Blockchain schema

---
+++ BlockchainModel.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var BlockchainSchema = new Schema({
+    index : {type:Number  ,required: true,unique = true },
+    timestamp: {type:Date, required: true},
+    previousHash: {type:String,required: true},
+    transactions: {type:Array, required: true}
+},{strict:false});
+
+module.exports = mongoose.model('Blockchain', BlockchainSchema);
